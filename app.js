23834eb8746185ec5c6355c4eed39ae571bf47c1
Wajih Ul Hasan <wajihulhasan01@gmail.com>
2024-11-26 23:59:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const a = document.querySelector("#img")
+function On(){
+    a.style.animationDuration = 1 + "s";
+}
+function Off(){
+    a.style.animationDuration = 0 + "s";
+}
+function high(){
+    a.style.animationDuration = 0.5 + "s";
+
+}
+function medium(){
+    a.style.animationDuration = 1.5 + "s";
+
+}
+function low(){
+    a.style.animationDuration = 2 + "s";
+
+}
