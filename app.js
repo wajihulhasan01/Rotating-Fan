922711233714a803cad99f0d06c48c66b13af092
wajihulhasan01 <wajihulhasan01@gmail.com>
2025-01-02 11:36:24 +0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-const a = document.querySelector("#img")
-function On(){
-    a.style.animationDuration = 1 + "s";
-}
-function Off(){
-    a.style.animationDuration = 0 + "s";
-}
-function high(){
-    a.style.animationDuration = 0.5 + "s";
+// const a = document.querySelector("#fan")
+// function On(){
+//     a.style.animationDuration = 1 + "s";
+// }
+// function Off(){
+//     a.style.animationDuration = 0 + "s";
+// }
+// function high(){
+//     a.style.animationDuration = 0.5 + "s";
 
-}
-function medium(){
-    a.style.animationDuration = 1.5 + "s";
+// }
+// function medium(){
+//     a.style.animationDuration = 1.5 + "s";
 
-}
-function low(){
-    a.style.animationDuration = 2 + "s";
+// }
+// function low(){
+//     a.style.animationDuration = 2 + "s";
 
+// }
+
+const fan = document.querySelector("#fan");
+
+function onFan() {
+  fan.style.animationDuration = "1s";
 }
+
+function offFan() {
+  fan.style.animationDuration = "0s";
+}
+
+function highFan() {
+  fan.style.animationDuration = "0.5s";
+}
+
+function mediumFan() {
+  fan.style.animationDuration = "1.5s";
+}
+
+function lowFan() {
+  fan.style.animationDuration = "2s";
+}
+
